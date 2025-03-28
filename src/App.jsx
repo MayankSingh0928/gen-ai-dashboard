9b2f507cdff0325712be9f27ce729c5fef9461a5
MayankSingh0928 <mayanksi0928@gmail.com>
2025-03-29 03:53:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,6 @@ const App = () => {
   return (
     <Provider store={store}>
       <div className="container mx-auto p-6">
-      <h1 className="text-1xl font-bold text-red-500">Hello, Tailwind!</h1>
-      <div className="bg-blue-500 text-white p-4 rounded-lg">
-        <h1>Hello</h1>
-      </div>
         <h1 className="text-2xl font-bold mb-4">Gen AI Analytics Dashboard</h1>
         <QueryInput />
         <QueryHistory />
